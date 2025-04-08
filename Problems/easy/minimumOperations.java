47446699e2abe2fe9955d82cd5bc1a336c5c2b14
Jinesh Singatkar <jineshsingatkar@gmail.com>
2025-04-08 23:10:09 +0530

    add minimumOperations solution for daily challenges

---
+++ Problems/easy/minimumOperations.java
@@ -0,0 +1,30 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class minimumOperations {
+    public int minimumOperations(int[] nums) {
+        int operations = 0;
+        int i = 0;
+
+        while (i < nums.length) {
+            Set<Integer> seen = new HashSet<>();
+            boolean hasDuplicate = false;
+            for (int j = i; j < nums.length; j++) {
+                if (seen.contains(nums[j])) {
+                    hasDuplicate = true;
+                    break;
+                }
+                seen.add(nums[j]);
+            }
+
+            if (!hasDuplicate) {
+                break;
+            }
+            operations++;
+            i += 3;
+        }
+
+        return operations;
+    }
+}
+
