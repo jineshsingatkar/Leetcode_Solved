531dff611dde738733c9e94c217c7f5e9ad324a1
Jinesh Singatkar <jineshsingatkar@gmail.com>
2025-04-09 21:15:53 +0530

    add minOperations implementation for calculating operations based on k

---
+++ Problems/easy/minOperations.java
@@ -0,0 +1,22 @@
+import java.util.HashSet;
+import java.util.Set;
+
+public class minOperations {
+    public int minOperations(int[] nums, int k) {
+         for (int num : nums) {
+            if (num < k) {
+                return -1; // Cannot increase values
+            }
+        }
+
+        Set<Integer> greaterThanK = new HashSet<>();
+        for (int num : nums) {
+            if (num > k) {
+                greaterThanK.add(num);
+            }
+        }
+
+        return greaterThanK.size();
+
+    }
+}
