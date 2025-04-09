5d7f05f527d8377bb461b31e585b34715709a694
Jinesh Singatkar <jineshsingatkar@gmail.com>
2025-04-09 21:14:24 +0530

    add minOperations solution for leetcode 3375

---
+++ dailyChallenges/leetcode3375.java
@@ -0,0 +1,19 @@
+public class leetcode3375 {
+    public int minOperations(int[] nums, int k) {
+        for (int num : nums) {
+           if (num < k) {
+               return -1; // Cannot increase values
+           }
+       }
+
+       Set<Integer> greaterThanK = new HashSet<>();
+       for (int num : nums) {
+           if (num > k) {
+               greaterThanK.add(num);
+           }
+       }
+
+       return greaterThanK.size();
+
+   }
+}
