24d4df6ae8bc6053ec51c031b9b3ef8b1b4bda49
Jinesh Singatkar <jineshsingatkar@gmail.com>
2025-04-13 09:55:56 +0530

    add countGoodNumbers class to calculate the number of good numbers based on even and odd positions

---
+++ Problems/medium/countGoodNumbers.java
@@ -0,0 +1,25 @@
+package Problems.medium;
+private static final int MOD = 1_000_000_007;
+public class countGoodNumbers {
+    long evenPositions = (n + 1) / 2;
+        long oddPositions = n / 2;
+
+        long evenWays = modPow(5, evenPositions, MOD);
+        long oddWays = modPow(4, oddPositions, MOD);
+
+        return (int) ((evenWays * oddWays) % MOD);
+    }
+
+    private long modPow(long base, long exp, int mod) {
+        long result = 1;
+        base %= mod;
+
+        while (exp > 0) {
+            if ((exp & 1) == 1) result = (result * base) % mod;
+            base = (base * base) % mod;
+            exp >>= 1;
+        }
+
+        return result;
+    }
+
