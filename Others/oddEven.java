8862226882e3228bf6ae92389bdfacb5c3f11523
Jinesh Singatkar <jineshsingatkar@gmail.com>
2025-04-16 20:49:43 +0530

    add oddEven class with methods to determine odd/even and convert between string and integer

---
+++ Others/oddEven.java
@@ -0,0 +1,22 @@
+package Others;
+
+public class oddEven {
+    public static void main(String[] args) {
+        int n = 5; // Example input
+        System.out.println(oddEven(n)); // Output: "Odd"
+    }
+
+    public static String oddEven(int n) {
+        return (n % 2 == 0) ? "Even" : "Odd";
+    }
+    public static String oddEven(int n) {
+        return (n % 2 == 0) ? "Even" : "Odd";
+    }
+    public static int stringToInteger(String str) {
+        return Integer.parseInt(str);
+    }
+    public static String integerToString(int num) {
+        return Integer.toString(num);
+    }
+
+}
